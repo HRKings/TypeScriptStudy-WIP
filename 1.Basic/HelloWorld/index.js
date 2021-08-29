1985840071b2d8e1ac38906ffdd84d2021a4e6d9
Almir Jr <almirafjr.dev@gmail.com>
2021-08-29 17:06:55 -0300

    Adding 1.Basic folder, .gitignore and Vscode workspace to .gitignore

---
+++ 1.Basic/HelloWorld/index.js
@@ -0,0 +1,3 @@
+var message; // -> Declaring variable 'message' as an type string
+message = "Hello World"; // -> Attributing the string "Hello World" to the variable 'message'
+console.log(message); // -> Printing the variable 'message' at the console log
---
+++ 1.Basic/HelloWorld/index.ts
@@ -0,0 +1,12 @@
+let message: string; // -> Declaring variable 'message' as an type string
+
+message = "Hello World"; // -> Attributing the string "Hello World" to the variable 'message'
+
+console.log(message); // -> Printing the variable 'message' at the console log
+
+/* => Type: tsc index.ts
+   -> This is going to compile the TypeScript File as an JavaScript File
+   
+   => Type node index.js
+   => This is going to execute your JavaScript File that used to be an TypeScript File
+*/
