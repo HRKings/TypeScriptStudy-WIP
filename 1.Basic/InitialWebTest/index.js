a6bd839ca0d626d7e793b811d875a8f20267f7cc
Helton Reis <hrkings@rhaizzing.com>
2021-09-01 17:47:16 -0300

    [feature] Add the result on the page

--- 1.Basic/InitialWebTest/index.js
+++ 1.Basic/InitialWebTest/index.js
@@ -1,13 +1,14 @@
-"use strict";
 //!Elements Block Start!!//
 //Sum Elements
 var input1Sum = document.getElementById("num1Sum");
 var input2Sum = document.getElementById("num2Sum");
 var buttonSum = document.getElementById("buttonSum");
+var result2Sum = document.getElementById("resultSum");
 //Subtraction Elements
 var input1Sub = document.getElementById("num1Sub");
 var input2Sub = document.getElementById("num2Sub");
 var buttonSub = document.getElementById("buttonSub");
+var result2Sub = document.getElementById("resultSub");
 //!!Elements Block End!!//
 //Sum Function
 function sum(a, b) {
@@ -19,10 +20,16 @@ function sub(a, b) {
 }
 //Sum button Event
 buttonSum === null || buttonSum === void 0 ? void 0 : buttonSum.addEventListener("click", function () {
-    console.log(sum(Number(input1Sum.value), Number(input2Sum.value)));
+    var result = sum(Number(input1Sum.value), Number(input2Sum.value));
+    if ((result2Sum === null || result2Sum === void 0 ? void 0 : result2Sum.textContent) !== null) {
+        result2Sum.textContent = String(result);
+    }
 });
 //Subtraction button Event
 buttonSub === null || buttonSub === void 0 ? void 0 : buttonSub.addEventListener("click", function () {
-    console.log(sub(Number(input1Sub.value), Number(input2Sub.value)));
+    var result = sub(Number(input1Sub.value), Number(input2Sub.value));
+    if ((result2Sub === null || result2Sub === void 0 ? void 0 : result2Sub.textContent) !== null) {
+        result2Sub.textContent = String(result);
+    }
 });
 console.log("Hi Log, this is an calculator application");
--- 1.Basic/InitialWebTest/index.ts
+++ 1.Basic/InitialWebTest/index.ts
@@ -1,12 +1,16 @@
 //!Elements Block Start!!//
+
 //Sum Elements
 const input1Sum = document.getElementById("num1Sum") as HTMLInputElement;
 const input2Sum = document.getElementById("num2Sum") as HTMLInputElement;
 const buttonSum = document.getElementById("buttonSum");
+const result2Sum = document.getElementById("resultSum")
 //Subtraction Elements
 const input1Sub = document.getElementById("num1Sub") as HTMLInputElement;
 const input2Sub = document.getElementById("num2Sub") as HTMLInputElement;
 const buttonSub = document.getElementById("buttonSub");
+const result2Sub = document.getElementById("resultSub");
+
 //!!Elements Block End!!//
 
 //Sum Function
@@ -20,11 +24,19 @@ function sub(a: number, b:number) {
 
 //Sum button Event
 buttonSum?.addEventListener("click", function(){
-    console.log(sum(Number(input1Sum.value), Number(input2Sum.value)))
+    const result = sum(Number(input1Sum.value), Number(input2Sum.value));
+
+    if (result2Sum?.textContent !== null) {
+        result2Sum!.textContent = String(result);
+    }
 })
 //Subtraction button Event
 buttonSub?.addEventListener("click", function(){
-    console.log(sub(Number(input1Sub.value), Number(input2Sub.value)))
+    const result = sub(Number(input1Sub.value), Number(input2Sub.value));
+    
+    if (result2Sub?.textContent !== null) {
+        result2Sub!.textContent = String(result);
+    }
 })
 
 console.log("Hi Log, this is an calculator application");
